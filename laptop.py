6b3680bffb9758ac8f4fc4fdf456f810b04a903d
Abhishek Wani <43184560+whoami02@users.noreply.github.com>
2019-08-16 23:19:22 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
 data = pd.read_csv('https://s3.amazonaws.com/hr-testcases/399/assets/trainingdata.txt')
 # data = pd.read_csv('trainingdata.txt')

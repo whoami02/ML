38c3289305f72e274d670210df0da11a54a1bdb3
Abhishek Wani <43184560+whoami02@users.noreply.github.com>
2021-10-09 14:09:29 +0530

    Update Assign1-0001.py

--- Assign1-0001.py
+++ Assign1-0001.py
@@ -11,39 +11,57 @@ import matplotlib.pyplot as plt
 
 class environment(object):
     prices = np.random.randint(100, 500, 50)
-    max_price_addon = random.randint(5,30)
-    print(prices)
-    print(max_price_addon)
+    max_price_addon = random.randint(0,30)
+    """  print(prices)
+    print(max_price_addon)"""
 
     def __init__(self):
         self.time = 0
-        self.paper = random.randint(10,31)
-        self.HistPaper = []
-        self.HistPrice = []
+        self.paper = random.randrange(25)
+        self.HistPaper = []                                 #Paper available in stock History
+        self.HistPrice = []                                 #Price History
 
-    def percept(self):
-        Price = self.prices[range(len(self.prices))] + self.max_price_addon
+    def initial_percept(self):
+        Price = self.prices[random.randrange(len(self.prices))] + random.randrange(self.max_price_addon)
         self.HistPrice.append(Price)
         self.HistPaper.append(self.paper)
         dict = {'Prices': Price, 'InStock':self.paper}
         return dict
 
     def do(self, action):
-        self.time = self.time+1
-        inflation = self.time*3
+        self.time = self.time + 1
+        inflation = self.time * 3
         B = action['buy_paper']
         self.paper = self.paper + B
         self.HistPaper.append(self.paper)
-        #Price = self.prices[]
+        #Price = self.prices[len(self.prices)]+inflation
+        Price = self.prices[self.time] + inflation + random.randrange(self.max_price_addon)
+        self.HistPrice.append(Price)
+        dict = {'Price': Price, 'InStock':self.paper}
+        return dict
 
-"""
-class Agent(agent):
-    def __init__(self):
-        pass
-"""
+
+class agent(Agent):
+    def __init__(self, env):
+        self.money_used = 0
+        percepts = self.env.initial_percept()
+        self.paper_price = percepts['Prices']
+        self.instock = percepts['InStock']
+
+    def go(self, n):
+        Number_of_papers_to_buy = random.randrange(env.dict['InStock'])
+        for i in range(n):
+            self.money_used = self.money + Number_of_papers_to_buy * self.paper_price
+            percepts = env.do({'buy_paper':Number_of_papers_to_buy})
+            self.temp = self.paper_price
+            self.paper_price = percepts['Prices']
+            self.temp = self.temp + (self.paper_price - self.temp)//2
+            self.instock = percepts['InStock']
 
 #Driver
 env = environment()
-print(env.percept())
+bot = agent(env)
+bot.go(20)
+"""print(env.initial_percept())"""
 #agent = Agent(env)
 
